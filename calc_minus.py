669bb55af8b531f5d2b4ef86ab713127f538bf02
haxxorkoom3k <haxxorkoom3k@users.noreply.github.com>
2023-04-20 20:55:20 +0500

    init

---
+++ calc_minus.py
@@ -0,0 +1,13 @@
+def calc(a, b):
+        return a - b
+
+def call_calc():
+    try:
+        a = float(input("Введите первое число: "))
+        b = float(input("Введите второе число: "))
+        result = calc(a, b)
+    except ValueError:
+        raise ValueError('одно из введённых чисел не число')
+    return result
+
+print(call_calc())
